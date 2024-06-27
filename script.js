6a8b4819627ad24e02fd2bfe3b3c41e71685cee6
darkstardust98 <165183152+darkstardust98@users.noreply.github.com>
2024-06-27 09:12:31 +0200

    FIXED a bug where the function createGrid was being created inside the wrong scope

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-document.getElementById("grid-container").innerHTML(
-    function createGrid() {
-        const gridSize = 15
-        for (i = 0, i <= gridSize, i++) {
-            for (j = 0, j <= gridSize, j++) {
-                var newDiv = document.createElement("div");
-                newDiv.id = "square";
-                document.body.appendChild(newDiv);
-            }
+document.getElementById("grid-container").innerHTML = createGrid();
+
+function createGrid() {
+    const gridSize = 15
+    for (i = 0; i <= gridSize; i++) {
+        for (j = 0; j <= gridSize; j++) {
+            var newDiv = document.createElement("div");
+            newDiv.id = "square";
+            document.body.appendChild(newDiv);
         }
     }
-)
+}
+
+
+console.log("test");
