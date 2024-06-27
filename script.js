8bc5e755577581b445e761d3dd2b940a78d5bb77
darkstardust98 <165183152+darkstardust98@users.noreply.github.com>
2024-06-27 12:11:42 +0200

    REMOVED old commented version of button logic

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// document.getElementById("prompt").addEventListener("click", function() {
-//     let userInput = prompt("Please enter you prefered size in squares per side: ");
-//     document.getElementById("grid-container").innerHTML = createGrid(userInput);
-// })
-
-
-// function createGrid() {
-//     let gridSize = 15;
-//     let grid = "";
-//     for (let i = 0; i <= gridSize; i++) {
-//         for (j = 0; j <= gridSize; j++) {
-//             grid += '<div class="square"></div>';
-//         }
-//         grid += '<br>';
-//     }
-//     return grid;
-// }
-
-// var elements = document.getElementsByClassName("square");
-
-// for (let i = 0; i < elements.length; i++) {
-//     //Making square black upon hover
-//     elements[i].addEventListener("mouseenter", function(event) {
-//         event.target.style.backgroundColor = "black";
-//     })
-//     //Making square white again upon hover end
-//     elements[i].addEventListener("mouseleave", function(event) {
-//         event.target.style.backgroundColor = "white";
-//     })
-// }   
-
 document.getElementById("prompt").addEventListener("click", function() {
     let userInput = prompt("Please enter your preferred size in squares per side: ");
     document.getElementById("grid-container").innerHTML = createGrid(userInput);
@@ -41,7 +10,7 @@ function createGrid(gridSize) {
         for (j = 0; j <= gridSize; j++) {
             grid += '<div class="square"></div>';
         }
-        grid += '<br>';
+        // grid += '<br>';
     }
     return grid;
 }
